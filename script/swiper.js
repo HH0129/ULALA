ab01fa5adb3f714ef199d91a0a4e133fb1f69654
HH0129 <hannah050129@gmail.com>
2024-10-28 15:40:03 +0900

    ULALA

--- script/swiper.js
+++ script/swiper.js
@@ -6,16 +6,36 @@ var MainSwiper = new Swiper(".mainSwiper", {
       disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
+      el: ".mainSwiper .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".mainSwiper .swiper-button-next",
+      prevEl: ".mainSwiper .swiper-button-prev",
     },
   });
 
   var SubSwiper = new Swiper(".subSwiper",{
 
   });
+
 // 첫번째 컨테이너 스와이퍼
+var SubSwiper = new wiper(".subSwiper", {
+  slidesPerView: 1.3, //모바일
+  spaceBetween: 20,
+  navigation: {
+      nextEl: ".subSwiper .swiper-button-next",
+      prevEl: ".subSwiper .swiper-button-prev",
+  },
+  breakpoints: {
+      768: {
+          slidesPerView: 2.3, //태블릿
+          spaceBetween: 20,
+      },
+      1280: {
+          slidesPerView: 5.3, //웹
+          slidesPerGroup:5.3,
+          spaceBetween: 20,
+      },  
+  },
+});
